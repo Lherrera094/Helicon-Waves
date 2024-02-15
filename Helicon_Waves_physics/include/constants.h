e40117e8a9277d82651fa24cf9ca37c243079f53
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2024-02-15 14:44:53 -0600

    Add files via upload

---
+++ Helicon_Waves_physics/include/constants.h
@@ -0,0 +1,15 @@
+/*This file contains the maint constants used 
+for the helicon waves physics code*/
+
+#ifndef CONSTANTS_H
+
+#define CONSTANTS_H
+
+#define m_e    9.10938356e-31   //Kg
+#define e      1.60217663e-19   //C
+#define eps_0  8.85418782e-12   //C^2/(Nm^2)
+#define pi     3.14159265
+#define mu_0   12.5663706e-7    //Tm/A
+#define c      2.99792458e8     //m/s  
+
+#endif
---
+++ Helicon_Waves_physics/include/plasma_params.h
@@ -0,0 +1,15 @@
+/*This file cointains the functions to obtain the main wave and plasma 
+characteristics functions*/
+
+#ifndef PLASMA_PARAMS_H
+
+#define PLASMA_PARAMS_H
+
+#include "constants.h"
+#include <stdio.h>
+
+double w_p(double n);
+double k_s(double w);
+
+
+#endif
