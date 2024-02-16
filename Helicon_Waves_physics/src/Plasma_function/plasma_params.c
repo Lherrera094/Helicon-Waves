1bd751cc97fc5200cec7694bff428b35329cf041
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2024-02-16 16:47:19 -0600

    Add files via upload

--- Helicon_Waves_physics/src/Plasma_function/plasma_params.c
+++ Helicon_Waves_physics/src/Plasma_function/plasma_params.c
@@ -5,9 +5,54 @@ characteristics functions*/
 #include "constants.h"
 #include <math.h>
 
+
+//Plasma frequency 
+//Receives:plasma density(n)
 double w_p(double n){
+
+    double w = (n*pow(e,2))/(eps_0*m_e);
+    return sqrt(w);
 	
-	//double w = (n + 2);
-	
-	return (n*pow(e,2))/(eps_0*m_e);
 }
+
+
+//Skin number of the Helicon wave
+//Receives: plasma frequency (w)
+double k_s(double w){
+	
+    return w/c;
+	
+}
+
+
+//Cyclotron frequency
+//Receives: external magnetic field (B)
+double w_c(double B){
+    
+    return (e*B)/m_e;
+     
+}
+
+
+//Wave Number
+//Receives: external magnetic field(B), plasma density(n) and wave frequency(w)
+double k_w(double B, double n, double w){
+    
+    double k = (w*n*mu_0*e)/B;
+    return sqrt(k);
+    
+}
+
+
+//Damping factor
+//Receives: Wave frequency(w), cyclotron frequency(wc), collision frequency(v)
+double delt(double w, double wc, double v){
+    int i = 1;
+    return (w+(i*v))/wc;
+    
+}
+
+
+
+
+
