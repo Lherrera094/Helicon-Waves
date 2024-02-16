cd4fbfce79e4393d1d87c167f312417c3ed23cc5
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2024-02-16 16:45:21 -0600

    Add files via upload

--- Helicon_Waves_physics/src/main.c
+++ Helicon_Waves_physics/src/main.c
@@ -16,14 +16,31 @@
 #include <stdio.h>
 #include "constants.h"
 #include "plasma_params.h"
+#include "create_folder.h"
 
  int main(void){
  
- 	//Print the constants
- 	double n = 15.0;
- 	double wp = w_p(n);
+ 	//Input values
+ 	double n_0 = 2.0e19;	//Plasma density [m^-3]
+        double B_0 = 1.5;	//External Magnetic Field [T]
+ 	double w = 27.12e6; 	//Wave frequency [Hz]
+ 	double v = 0;		//Collision Frequency
+ 	
+ 	//Basic Parameters 
+ 	double wp = w_p(n_0);	//Plasma Frequency [rad/s]
+ 	double wc = w_c(B_0);	//Cyclotron Frequency
+ 	
+ 	//Creates folder Results
+ 	results_folder();
+ 	
+ 	//Creates folder to save files from run
+ 	plasma_files_folder();
 	
-	printf("Plasma Frequency = %.10e \n", wp);
+	//printf("Plasma Frequency = %.10e \n", wp);
+	//printf("Cyclotron Frequency = %.10e \n", wc);
+	//printf("Skin Number = %.10e \n", k_s(wp));
+	//printf("Wave Number = %f \n", k_w(B_0, n_0, w));
+	//printf("Damping Factor = %.10e \n", delt(w, wc, v));
 	 	
  	return 0;
  
