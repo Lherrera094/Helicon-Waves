b2d5874aff0a2a6790a1a4e42df1a759d23a5034
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2024-02-17 15:25:00 -0600

    Add files via upload

--- Helicon_Waves_physics/src/main.c
+++ Helicon_Waves_physics/src/main.c
@@ -14,34 +14,58 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "constants.h"
 #include "plasma_params.h"
 #include "create_folder.h"
+#include "write_file.h"
 
  int main(void){
  
  	//Input values
- 	double n_0 = 2.0e19;	//Plasma density [m^-3]
-        double B_0 = 1.5;	//External Magnetic Field [T]
- 	double w = 27.12e6; 	//Wave frequency [Hz]
- 	double v = 0;		//Collision Frequency
+ 	double n_0 = 2.0e19;		//Plasma density [m^-3]
+        double B_0 = 100e-4;		//External Magnetic Field [T]
+ 	double w = 27.12e6; 		//Wave frequency [Hz]
+ 	double v = 0;			//Collision Frequency
  	
- 	//Basic Parameters 
- 	double wp = w_p(n_0);	//Plasma Frequency [rad/s]
- 	double wc = w_c(B_0);	//Cyclotron Frequency
+ 	double input_values[] = {n_0, B_0, w, v};	//Array saves input values
+ 	
+ 	//Main Plasma Parameters 
+ 	double wp = w_p(n_0);		//Plasma Frequency [rad/s]
+ 	double wc = w_c(B_0);		//Cyclotron Frequency [rad/s]
+ 	
+ 	double plas_par[] = {wp, wc};
+ 	
+ 	//Wave Characteristics
+ 	double del = delt(w, wc, v);	//Damping Factor
+ 	double K_s = k_s(wp);		//Skin number
+ 	double K_w = k_w(B_0, n_0, w);	//Wave number
+ 	double K_min = k_min(del,K_s);	//k minimum
+ 	double K_max = k_max(del,K_s);	//k maximum
+ 	
+ 	double wave_char[] = {K_w, K_s, K_min, K_max, del};
  	
  	//Creates folder Results
  	results_folder();
  	
  	//Creates folder to save files from run
- 	plasma_files_folder();
+ 	char* foldername = plasma_files_folder();
+ 	
+ 	//Writes file with plasma parameters
+ 	write_plasma_params(input_values, plas_par, wave_char, foldername);
 	
-	//printf("Plasma Frequency = %.10e \n", wp);
-	//printf("Cyclotron Frequency = %.10e \n", wc);
-	//printf("Skin Number = %.10e \n", k_s(wp));
-	//printf("Wave Number = %f \n", k_w(B_0, n_0, w));
-	//printf("Damping Factor = %.10e \n", delt(w, wc, v));
-	 	
+	/*printf("Plasma Frequency = %.3e rads/s \n", wp);
+	printf("Cyclotron Frequency = %.3e rads/s\n", wc);
+	printf("Skin Number = %.3e m^-1 \n", K_s);
+	printf("Wave Number = %f m^-1\n", k_w(B_0, n_0, w));
+	printf("Damping Factor = %.3e \n", del);
+	printf("The quotient of frequencies is %f \n", wc/w);
+	printf("k_min = %f m^-1 \n", k_min(del,K_s));
+	printf("k_max = %f m^-1\n", k_max(del,K_s));
+	*/
+	
+	
+	free(foldername); 	
  	return 0;
  
  }
