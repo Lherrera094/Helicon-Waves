c73c36a2f36facc97bf0e43be6c2cd2b9165244d
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2024-02-18 16:00:44 -0600

    Add files via upload

--- Helicon_Waves_physics/include/constants.h
+++ Helicon_Waves_physics/include/constants.h
@@ -5,11 +5,12 @@ for the helicon waves physics code*/
 
 #define CONSTANTS_H
 
-#define m_e    9.10938356e-31   //Kg
-#define e      1.60217663e-19   //C
-#define eps_0  8.85418782e-12   //C^2/(Nm^2)
-#define pi     3.14159265
-#define mu_0   12.5663706e-7    //Tm/A
-#define c      2.99792458e8     //m/s  
+#define m_e    9.10938356e-31   //electron mass [Kg]
+#define m_i    1.67262192e-27	//Proton mass [Kg]
+#define e      1.60217663e-19   //Electric charge [C]
+#define eps_0  8.85418782e-12   //Electric permittivity in vacuum [C^2/(Nm^2)]
+#define pi     3.14159265	//pi
+#define mu_0   12.5663706e-7    //Magnetic permeability [Tm/A]
+#define c      2.99792458e8     //lLight speed [m/s]  
 
 #endif
---
+++ Helicon_Waves_physics/include/read_json.h
@@ -0,0 +1,7 @@
+/*This files contains the functions to read a the JSON file with the input values*/
+
+
+#include "cJSON.h"
+
+double read_input_numJSON();
+char* read_input_filenameJSON();
